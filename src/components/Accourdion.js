38564dc88aa50b8500c0c0c3dba4dfde0bfd8bcf
Sarang-17 <sarang.manerikar.ece20@itbhu.ac.in>
2023-05-18 10:46:44 +0530

    Some of the '/product' todos

--- src/components/Accourdion.js
+++ src/components/Accourdion.js
@@ -9,12 +9,13 @@ const Accourdion = ({ defaulIsOpen, children, title }) => {
     <div classNames={" transition-all duration-300"}>
       <div
         className=" flex items-center justify-between py-2 cursor-pointer"
-        onClick={() => setIsOpen((o) => !o)}>
+        onClick={() => setIsOpen((o) => !o)}
+      >
         <h1 className=" text-sm xl:text-[22px] font-semibold">{title}</h1>
         <div className="hidden xl:flex">
           <ExpandLess
             className={classNames(
-              "-mr-1 ml-2 h-5 w-5 mt-1 transition-all duration-300 xl:w-10 xl:h-10 xl:bg-white",
+              "-mr-1 ml-2 h-5 w-5 mt-1 transition-ease-in-out duration-300 xl:w-10 xl:h-10 xl:bg-white",
               !isOpen ? "rotate-180" : ""
             )}
             fontSize={"large"}
